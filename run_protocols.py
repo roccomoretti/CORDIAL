33924472956b048b3d3cbb3d490b7738dd7f6d9c
Rocco Moretti <rmorettiase@gmail.com>
2026-05-04 11:50:29 -0500

    Keep old entry point

---
+++ run_protocols.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Run CORDIAL protein-ligand binding affinity prediction.
+
+Author: Benjamin P. Brown, MD, PhD
+Email: benjamin.p.brown@vanderbilt.edu
+Date: 05/06/2024
+
+"""
+# Project imports
+from CORDIAL.main import main
+
+# Execute main() when run from command line
+if __name__ == '__main__':
+    main()
